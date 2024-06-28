d6b9b08c0eb07a8bbc42fcff809faf596885f846
jjh <jjh@DESKTOP-V655TTC>
2024-06-28 09:15:40 +0900

    초기 커밋.

---
+++ Int8.cs
@@ -0,0 +1,14 @@
+﻿namespace std_msgs.msg
+{
+    public class Int8 : Message
+    {
+        public sbyte data;
+
+        public override string ToString()
+        {
+            return $"Int8(data={data})";
+        }
+
+        public override string MessageType => "std_msgs/msg/Int8";
+    }
+}
